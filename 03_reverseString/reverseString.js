5cfce83c6b3025d5607dfa0b6b6e68358673d90c
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-20 17:01:12 +0200

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringToArray = string.split('');
+    const arrayLength = stringToArray.length;
+    const newArray = [];
 
+    for (let i = arrayLength; i > 0; --i) {
+        newArray.push(stringToArray[i-1]);
+    };
+    const newText = newArray.join('');
+    return newText
 };
 
+reverseString('cars are big');
+// reverseString(1)
+// reverseString('')
+// reverseString(' ')
 // Do not edit below this line
 module.exports = reverseString;
