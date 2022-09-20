9d9bf38eb46407af0ba7317d64217c9868680ef3
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-20 16:14:53 +0200

    completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str = ''
+    if (!(num > -1)) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; ++i) {
+        str += string;
+    }
+    return str
 };
-
+repeatString('hey', 0)
 // Do not edit below this line
 module.exports = repeatString;
