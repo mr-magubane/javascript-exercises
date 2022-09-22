3b62f43efcb45bf278c29a9c078c128282b6b7e7
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-22 21:28:13 +0200

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if ( start < 0 || !Number.isInteger(start) ||  end < 0 || !Number.isInteger(end)) {
+        return 'ERROR';
+    };
+    
+    if (start > end) {
+        let placeholder = start;
+        start = end;
+        end = placeholder;
+    }
 
+    let sum = 0;
+    for (let i = start; i < end + 1; ++i) {
+        sum = sum + i;
+    };
+
+    return sum;
 };
 
 // Do not edit below this line
