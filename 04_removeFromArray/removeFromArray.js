b923e39224a482e5675f3ac11adcd1adfce624ac
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-22 18:19:16 +0200

    completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
 
+    let Array = arguments[0];
+    for (let i = arguments.length-1; i > 0; --i) {
+        let toRemove = arguments[i];
+        
+        Array = Array.filter(function (a) {
+            return a !== toRemove;
+        });
+    };
+    return Array;
 };
 
+removeFromArray([7,8,8], 7, 9, 6);
 // Do not edit below this line
 module.exports = removeFromArray;
