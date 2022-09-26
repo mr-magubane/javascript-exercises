4fdfcc2ef8941a9ed1eeba19379972c47674f95d
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-26 19:39:37 +0200

    completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrayOfBooks) {
+    bookTitles = [];
 
+    for (let i = 0; i < arrayOfBooks.length; ++i) {
+      bookTitles.push(arrayOfBooks[i]['title']);
+    }
+    return bookTitles;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
