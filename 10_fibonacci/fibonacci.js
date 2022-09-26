dc3d5a7174f0a46828ec0d21e8bb018ffd183595
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-26 15:17:55 +0200

    completed exercise 09

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number);
+    if (number < 0) {
+        return "OOPS";
+    };
+    if (number === 1) {
+        return 1;
+    };
+    if (number === 0) {
+        return 0;
+    };
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
