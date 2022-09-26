7f8a1ba264ec37d3758c3857af57c155be3ec369
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-26 15:00:19 +0200

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    let noPunctuation = "";
+    let reverse = "";
 
+    for (let i = 0; i < string.length; ++i) {
+        if (string[i].toUpperCase() === string[i].toLowerCase()) {
+            continue;
+        } else {
+            noPunctuation += string[i].toLowerCase();
+            //reverse += string[string.length - i];
+        }
+    }
+    for (let i = 0; i < noPunctuation.length; ++i) {
+        reverse += noPunctuation[noPunctuation.length - 1 - i];
+    }
+    return noPunctuation === reverse;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
