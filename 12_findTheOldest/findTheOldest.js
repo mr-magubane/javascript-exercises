c33e6774fbe316975b9f3cef0bf0c4e78b29d7ca
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-27 19:57:21 +0200

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    Oldest = 0;
+    index = 0;
+    for (let i = 0; i < people.length; ++i) {
+        if (people[i]['yearOfDeath'] === undefined) {
+            death = new Date(). getFullYear();
+        } else {
+            death = people[i]['yearOfDeath'];
+        }
 
+        age = death - people[i]['yearOfBirth'];
+        if (age > Oldest) {
+            Oldest = age;
+            index = i;
+            continue;
+        };
+        Oldest = age;
+    }
+
+    return people[index];
 };
 
 // Do not edit below this line
