6cf9bae242985825b923f6a608fbb455053eea1e
Mlondi Magubane <mlondimagubane@gmal.com>
2022-09-28 20:54:27 +0200

    completed exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,25 @@
-const caesar = function() {
+const caesar = function(cipher, shift) {
+    if (shift < 0) { //To handle negative shifts
+        shift += 26;
+    };
 
+    var message = ''; 
+
+    for (let i = 0; i < cipher.length; ++i) {
+        if (cipher[i].toUpperCase() !== cipher[i].toLowerCase()) { //Is the character a letter
+            if (cipher[i] == cipher[i].toUpperCase()) {
+                const charUnicode = cipher.charCodeAt(i) + shift;
+                message += String.fromCharCode(((charUnicode - 65)%26) + 65);
+                continue;
+            };
+            const charUnicode = cipher.charCodeAt(i) + shift;
+                message += String.fromCharCode(((charUnicode - 97)%26) + 97);
+                continue;
+        };
+        message += cipher[i];
+    };
+
+    return message;
 };
 
 // Do not edit below this line
